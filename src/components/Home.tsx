da808def09021f3833ecdb707e3ada51e52a0a1e
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2023-10-25 21:44:26 +0200

    Adding ReactQuery and setting it up

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,42 @@
+import { useQuery } from '@tanstack/react-query'
 import { FunctionComponent } from 'react'
 
+interface Post {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+const fetchPosts = async () => {
+  return await fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json())
+}
+
 const Home: FunctionComponent = () => {
-  return <section className='home'>Home</section>
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  })
+
+  if (isPending) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
+  return (
+    <section className='home'>
+      {data.map((post: Post) => {
+        return (
+          <>
+            <div>{post.title}</div>
+          </>
+        )
+      })}
+    </section>
+  )
 }
 
 export default Home
