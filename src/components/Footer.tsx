31b4cd1659275a8f0ffdaaa4e67b9c0cd83fd6b1
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2023-11-11 17:53:51 +0100

    adjusting styles in routeError & footer components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from 'react'
 
 const Footer: FunctionComponent = () => {
   return (
-    <footer className='bg-gray-800 w-full h-12 flex items-center justify-center text-white'>
+    <footer className='bg-gray-800 w-full h-12 flex items-center justify-center text-white text-sm'>
       Copyright &copy; 2023 brzoskwi
     </footer>
   )
--- src/components/RouteError.tsx
+++ src/components/RouteError.tsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom'
+
 const RouteError = () => {
   return (
-    <main className='w-full h-screen flex items-center justify-center'>
-      OOPS! <br /> PAGE NOT FOUND. REDIRECTION TBD
+    <main className='w-full h-screen flex flex-col items-center justify-center'>
+      <aside>OOPS!</aside>
+      <section className='flex flex-col items-center justify-center'>
+        <h2 className='text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight'>
+          PAGE NOT FOUND!
+        </h2>
+        <Link
+          to='/'
+          type='button'
+          className='inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+          BACK TO HOMEPAGE
+        </Link>
+      </section>
     </main>
   )
 }
