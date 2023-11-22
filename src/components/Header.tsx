64d0b2d2a0de18ee3455c629db9c3ed63d6e3325
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2023-11-22 01:17:52 +0100

    rwd for menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Navigation from './Navigation'
 
 const Header: FunctionComponent = () => {
   return (
-    <header className='header col-start-1 col-end-2 row-span-2'>
+    <header className='header col-start-1 col-end-3 w-[244px] row-span-2'>
       <Navigation />
     </header>
   )
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,76 +1,72 @@
 import { FunctionComponent } from 'react'
-import { Disclosure } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { classNames } from '../utils/helpers'
-
-const navigation = [{ name: 'Dashboard', href: '#', current: true }]
+import { Link, useLocation } from 'react-router-dom'
+import { NavigationItem } from '../types/types'
+import {
+  IoHomeOutline,
+  IoHomeSharp,
+  IoSearchOutline,
+  IoSearchSharp,
+  IoLogOut,
+} from 'react-icons/io5'
+import CircleIcon from '../components/CircleIcon'
+import { IoIosLogOut } from 'react-icons/io'
 
 const Navigation: FunctionComponent = () => {
-  return (
-    <Disclosure as='nav' className='bg-gray-800 w-full'>
-      {({ open }) => (
-        <>
-          <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-            <div className='relative flex h-16 items-center justify-between'>
-              <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
-                <Disclosure.Button className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
-                  <span className='absolute -inset-0.5' />
-                  <span className='sr-only'>Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
-                  ) : (
-                    <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
-                <div className='flex flex-shrink-0 items-center'>
-                  <img className='h-8 w-auto' src='./assets/logo.png' alt='WSEI' />
-                </div>
-                <div className='hidden sm:ml-6 sm:block'>
-                  <div className='flex space-x-4'>
-                    {navigation.map(item => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}>
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+  const location = useLocation()
 
-          <Disclosure.Panel className='sm:hidden'>
-            <div className='space-y-1 px-2 pb-3 pt-2'>
-              {navigation.map(item => (
-                <Disclosure.Button
-                  key={item.name}
-                  as='a'
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}>
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+  const navigation: NavigationItem[] = [
+    {
+      name: 'Feed',
+      href: '/feed',
+      iconBase: <IoHomeOutline className='text-2xl mr-2' />,
+      iconActive: <IoHomeSharp className='text-2xl mr-2' />,
+    },
+    {
+      name: 'Explore',
+      href: '/explore',
+      iconBase: <IoSearchOutline className='text-2xl mr-2' />,
+      iconActive: <IoSearchSharp className='text-2xl mr-2' />,
+    },
+    {
+      name: 'Profile',
+      href: '/profile',
+      iconBase: <CircleIcon imageUrl='./assets/profile.jpg' />,
+      iconActive: <CircleIcon imageUrl='./assets/profile.jpg' />,
+      current: true,
+    },
+    {
+      name: 'Logout',
+      href: '/logout',
+      iconBase: <IoIosLogOut className='text-2xl mr-2' />,
+      iconActive: <IoLogOut className='text-2xl mr-2' />,
+    },
+  ]
+
+  return (
+    <nav className='h-full bg-gray-800 text-white flex flex-col w-[72px] md:w-[244px]'>
+      <aside className='flex flex-row items-center justify-center w-full h-24'>
+        <img className='h-10 w-auto' src='./assets/logo.png' alt='WSEI' />
+      </aside>
+      <section className='flex flex-col flex-grow justify-start'>
+        {navigation.map((item, index) => (
+          <Link
+            key={item.name}
+            to={item.href}
+            className={classNames(
+              item.current ? 'font-bold text-white' : 'font-medium text-gray-300 mt-0.5',
+              'ml-1 md:ml-2 flex flex-row justify-start align-middle text-center hover:bg-gray-700 hover:text-white rounded-md px-5 py-2 text-sm font-medium w-11/12 hover:cursor-pointer'
+            )}
+            aria-current={item.current ? 'page' : undefined}
+            style={{ marginTop: index === navigation.length - 1 ? 'auto' : undefined }}>
+            <span className='my-auto'>
+              {location.pathname === item.href ? item.iconActive : item.iconBase}
+            </span>
+            <span className='ml-1 my-2 invisible md:visible'>{item.name}</span>
+          </Link>
+        ))}
+      </section>
+    </nav>
   )
 }
 
