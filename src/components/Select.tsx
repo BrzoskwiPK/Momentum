bb05885e3240f5960d15f19ed699025dd62c7925
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2023-12-27 14:43:15 +0100

    fixing bug with setting initial value for parent component

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,50 +1,49 @@
-import { FC, useCallback } from 'react'
+import { FC, useCallback, useEffect, useState } from 'react'
 import Option from './Option'
 import { OptionProps } from '../types/types'
 import React from 'react'
 
 interface SelectProps {
   fieldName: string
-  options?: OptionProps[]
-  setCurrentValue?: (value: string) => void
+  options: OptionProps[]
+  setCurrentValue: (value: string) => void
 }
 
-const arePropsEqual = (prevProps: SelectProps, nextProps: SelectProps) => {
+const Select: FC<SelectProps> = ({ fieldName, options, setCurrentValue }: SelectProps) => {
+  const [isDefaultValueSet, setIsDefaultValueSet] = useState(false)
+
+  const handleOnChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      const selectedValue = event.target.value
+      if (setCurrentValue) {
+        setCurrentValue(selectedValue)
+      }
+    },
+    [setCurrentValue]
+  )
+
+  useEffect(() => {
+    if (!isDefaultValueSet && options && options.length > 0) {
+      const firstOptionValue = options[0].value
+      setCurrentValue && setCurrentValue(firstOptionValue)
+      setIsDefaultValueSet(true)
+    }
+  }, [options, setCurrentValue, isDefaultValueSet])
+
   return (
-    prevProps.fieldName === nextProps.fieldName &&
-    prevProps.setCurrentValue === nextProps.setCurrentValue &&
-    prevProps.options === nextProps.options
+    <>
+      <select
+        id={fieldName}
+        className='bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+        onChange={handleOnChange}>
+        {options
+          ? options.map((option, index) => (
+              <Option key={index} value={option.value} label={option.label} />
+            ))
+          : null}
+      </select>
+    </>
   )
 }
-
-const Select: FC<SelectProps> = React.memo(
-  ({ fieldName, options, setCurrentValue }: SelectProps) => {
-    const handleOnChange = useCallback(
-      (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const selectedValue = event.target.value
-        if (setCurrentValue) {
-          setCurrentValue(selectedValue)
-        }
-      },
-      [setCurrentValue]
-    )
-
-    return (
-      <>
-        <select
-          id={fieldName}
-          className='bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-          onChange={handleOnChange}>
-          {options
-            ? options.map((option, index) => (
-                <Option key={index} value={option.value} label={option.label} />
-              ))
-            : null}
-        </select>
-      </>
-    )
-  },
-  arePropsEqual
-)
 
 export default Select
