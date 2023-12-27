d89517f5a7149b39b0e3ff67d09c44228fe731f2
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2023-12-27 14:41:17 +0100

    introducing loader bar

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,11 @@ import { FC } from 'react'
 import { BarLoader } from 'react-spinners'
 
 const Loading: FC = () => {
-  return <BarLoader color='#6366f1' width={500} />
+  return (
+    <div className='w-[100vw] h-[100vh] flex justify-center items-center'>
+      <BarLoader color='#6366f1' width={500} />
+    </div>
+  )
 }
 
 export default Loading
