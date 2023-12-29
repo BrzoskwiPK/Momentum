bb0fd9d6351b6a2740c26b9b85363b97475fd6ec
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2023-12-29 17:52:49 +0100

    single responsibility principle for posts page

--- src/components/pages/Posts.tsx
+++ src/components/pages/Posts.tsx
@@ -1,15 +1,11 @@
-import { FC, useCallback, useState } from 'react'
+import { FC, useState } from 'react'
 import { usePosts } from '../../hooks/usePosts'
 import PageHeader from '../PageHeader'
 import PostsGrid from '../PostsGrid'
 import AddPostForm from '../forms/AddPostForm'
-import { Post } from '../../types/types'
-import { useQueryClient } from '@tanstack/react-query'
-import { createPost } from '../../api/posts'
-import { deletePost as deletePostFn } from '../../api/posts'
 
 const Posts: FC = () => {
-  const { posts, fetchData } = usePosts()
+  const { posts, deletePost, addPost } = usePosts()
   const [showPostForm, setShowPostForm] = useState<boolean>(false)
 
   const openPostForm = () => {
@@ -18,40 +14,6 @@ const Posts: FC = () => {
 
   const closePhotoForm = () => {
     setShowPostForm(false)
-  }
-
-  const queryClient = useQueryClient()
-
-  const deletePost = useCallback(
-    async (postId: number) => {
-      await queryClient.fetchQuery({
-        queryKey: [`deletePost-${postId}`],
-        queryFn: () => deletePostFn(postId),
-      })
-
-      const prevPosts = queryClient.getQueryData<Post[]>(['posts']) || []
-
-      queryClient.setQueryData(['posts'], [...prevPosts.filter(post => post.id !== postId)])
-
-      queryClient.invalidateQueries({ queryKey: ['posts'] })
-      fetchData()
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  )
-
-  const addPost = async (post: Post) => {
-    await queryClient.fetchQuery({
-      queryKey: [`publishPost-${post.id}`],
-      queryFn: () => createPost(post),
-    })
-
-    const prevPosts = queryClient.getQueryData<Post[]>(['posts']) || []
-
-    queryClient.setQueryData(['posts'], [...prevPosts, post])
-
-    queryClient.invalidateQueries({ queryKey: ['posts'] })
-    fetchData()
   }
 
   return (
--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,9 +1,46 @@
 import { Post } from '../types/types'
 import { useData } from './useData'
-import { fetchAllPosts } from '../api/posts'
+import { createPost, deletePost as deletePostFn, fetchAllPosts } from '../api/posts'
+import { useCallback } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
 
 export const usePosts = () => {
   const { data: posts, fetchData } = useData<Post>({ queryKey: ['posts'], fetchFn: fetchAllPosts })
 
-  return { posts, fetchData }
+  const queryClient = useQueryClient()
+
+  const deletePost = useCallback(
+    async (postId: number) => {
+      await queryClient.fetchQuery({
+        queryKey: [`deletePost-${postId}`],
+        queryFn: () => deletePostFn(postId),
+      })
+
+      const prevPosts = queryClient.getQueryData<Post[]>(['posts']) || []
+
+      queryClient.setQueryData(['posts'], [...prevPosts.filter(post => post.id !== postId)])
+
+      queryClient.invalidateQueries({ queryKey: ['posts'] })
+      fetchData()
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  )
+
+  const addPost = async (post: Post) => {
+    await queryClient.fetchQuery({
+      queryKey: [`publishPost-${post.id}`],
+      queryFn: () => createPost(post),
+    })
+
+    const prevPosts = queryClient.getQueryData<Post[]>(['posts']) || []
+
+    queryClient.setQueryData(['posts'], [...prevPosts, post])
+
+    queryClient.invalidateQueries({ queryKey: ['posts'] })
+
+    fetchData()
+  }
+
+  return { posts, fetchData, deletePost, addPost }
 }
