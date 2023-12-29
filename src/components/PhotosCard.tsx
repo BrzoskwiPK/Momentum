8f01d169f2a3726a515cf900ccb9cc65834eb7f2
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2023-12-29 17:42:34 +0100

    fixing onClick placement

--- src/components/PhotosCard.tsx
+++ src/components/PhotosCard.tsx
@@ -9,15 +9,15 @@ interface PhotosCardProps {
 
 const PhotosCard: FC<PhotosCardProps> = ({ photo, onClick }: PhotosCardProps) => {
   return (
-    <div
-      className='relative h-[200px] w-[200px] my-5'
-      onClick={onClick ? () => onClick(photo.id) : undefined}>
+    <div className='relative h-[200px] w-[200px] my-5'>
       <img src={photo.url} alt='' className='h-full w-full object-cover' />
       <div className='absolute top-0 left-0 text-white z-10 pl-2 pt-1'>
         <p>{photo.title}</p>
       </div>
       {onClick ? (
-        <div className='absolute w-full bottom-0 text-white hover:cursor-pointer flex justify-end'>
+        <div
+          onClick={onClick ? () => onClick(photo.id) : undefined}
+          className='absolute w-full bottom-0 text-white hover:cursor-pointer flex justify-end'>
           <MdDelete className='w-6 h-6' />
         </div>
       ) : null}
