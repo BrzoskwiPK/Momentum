47117e40509bdcb39e3fe899a5d6fd85765a35b8
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2023-12-29 18:21:59 +0100

    adding margin to save button

--- src/components/forms/AddPostForm.tsx
+++ src/components/forms/AddPostForm.tsx
@@ -107,7 +107,7 @@ const AddPostForm: FC<AddPostFormProps> = ({
                 type='submit'
                 className={`${
                   title && content ? 'hover:bg-indigo-500' : 'disabled pointer-events-none'
-                } leading-6 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}>
+                } ml-4 leading-6 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}>
                 Save
               </button>
             </div>
