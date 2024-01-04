b1a9624ccf3fe27eff3ff7b93f04eab73bce1894
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2024-01-04 17:56:41 +0100

    redirection to feed page from the main logo in nav

--- src/components/NavLogo.tsx
+++ src/components/NavLogo.tsx
@@ -1,9 +1,19 @@
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const NavLogo: FC = () => {
+  const navigate = useNavigate()
+
+  const redirectToFeedPage = () => navigate('/feed')
+
   return (
     <aside className='flex flex-row items-center justify-center w-full h-24 max-[400px]:hidden'>
-      <img className='h-10 w-auto' src='./assets/logo.png' alt='WSEI' />
+      <img
+        onClick={redirectToFeedPage}
+        className='h-10 w-auto hover:cursor-pointer'
+        src='./assets/logo.png'
+        alt='WSEI'
+      />
     </aside>
   )
 }
