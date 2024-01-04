0148b527321169f628bcaf907b87a77a3dee01ff
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2024-01-04 18:39:02 +0100

    adding hiperlinks

--- src/components/UserContactDetails.tsx
+++ src/components/UserContactDetails.tsx
@@ -9,7 +9,22 @@ const UserContactDetails: FC<UserContactDetailsProps> = ({ user }) => (
   <div>
     {user?.website || user?.phone ? (
       <>
-        <strong>Contact:</strong> {user.website} or {user.phone}
+        <strong>Contact:</strong>{' '}
+        {
+          <a
+            className='underline underline-offset-1 text-cyan-600'
+            target='_blank'
+            href={'http://' + user.website}
+            rel='noreferrer'>
+            {user.website}
+          </a>
+        }{' '}
+        or{' '}
+        {
+          <a className='underline underline-offset-1 text-cyan-600' href={`tel:${user.phone}`}>
+            {user.phone}
+          </a>
+        }
       </>
     ) : null}
   </div>
